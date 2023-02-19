b68c767b67d362665ae9382064b59c2cfc1d4e50
Abhishek Sahani <sahani199890@gmail.com>
2023-02-19 20:49:54 +0530

    Weekend test - 3

--- src/main/java/com/example/ecommerce/controller/OrderController.java
+++ src/main/java/com/example/ecommerce/controller/OrderController.java
@@ -3,10 +3,9 @@ package com.example.ecommerce.controller;
 import com.example.ecommerce.model.Order;
 import com.example.ecommerce.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("ecommerce-order")
@@ -15,8 +14,11 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
-
-
+    @PostMapping("/place-order")
+    public ResponseEntity<Order> placeOrder(@RequestBody Order order ){
+        orderService.placeOrder(order);
+        return new ResponseEntity<>(order, HttpStatus.CREATED);
+    }
     @GetMapping("/get-order")
     public Order getOrder(@RequestParam Integer orderId){
         return orderService.getOrder(orderId);
